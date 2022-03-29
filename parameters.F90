0b74f1d5f91f5ada50ea364216776e18d6f6ac62
Rahul Kumar <rahuliitk@gmail.com>
2022-03-29 09:58:29 -0400

    Add files via upload

--- parameters.F90
+++ parameters.F90
@@ -5,22 +5,22 @@ module parameters
 !integer, parameter :: psn=selected_real_kind(15,307)  
 integer, parameter :: psn=kind(1.0e0)!precision of the simulation 
 integer, parameter :: nx=128!32*8!*16*8!no. of grid cells along x  
-integer, parameter :: ny=32!!32*8!no. of grid cells along y 
-integer, parameter :: nz=32!*16!no. of grid cells along z (used only if THREE_DIMS=y (3D simulation) is defined in the Makefile)
+integer, parameter :: ny=128!!32*8!no. of grid cells along y 
+integer, parameter :: nz=4!*16!no. of grid cells along z (used only if THREE_DIMS=y (3D simulation) is defined in the Makefile)
 integer, parameter :: nSubDomainsX=2!*16!*2!32 ! no. of sub-domains along x
 integer, parameter :: nSubDomainsY=2!8!*2! no. of sub-domains along y
 integer, parameter :: nSubDomainsZ=1! no. of sub-domains along z (relevant only for 3D)
-integer, parameter :: tfinish=2000000! total number of time steps
+integer, parameter :: tfinish=200000! total number of time steps
 
 ! physical parameters 
 real(psn), parameter :: g0=1.0_psn!if a relativistic plasma, g0 is the Lorentz factor of particles (it only sets the electron skin-depth) 
-integer, parameter   :: epc=32!number of the electrons per cell 
+integer, parameter   :: epc=32*2!number of the electrons per cell 
 real(psn), parameter :: mi=32.0_psn ! mass of ions
 real(psn), parameter :: me=1.0_psn ! mass of electrons
 real(psn), parameter :: c_ompe=10.0_psn ! no. of cells per electron skin-depth 
 real(psn), parameter :: c=0.45_psn! *0.098 ! speed of light (photons propagate about a distance c in one time step)
 real(psn), parameter :: EMspeedBoost=1.0_psn! EM waves are propagated faster than c by this factor 
-integer , parameter  :: curr_filter=8!3!no. of times the urrent filter (moving average) is applied  
+integer , parameter  :: curr_filter=0!3!no. of times the urrent filter (moving average) is applied  
 integer , parameter  :: nMoverEMfilter=0!8!no. of times the EM fld averaging is done, only relavant for the test particles 
 
 !------------------------------------------------------------------------------------------------
@@ -28,10 +28,10 @@ integer , parameter  :: nMoverEMfilter=0!8!no. of times the EM fld averaging is 
 !------------------------------------------------------------------------------------------------
 ! Set the period to "-1" to disable saving a particular data, e.g. "fld_save_period=-1" will disble saving the field data   
 character(len=256) :: data_folder=trim("data") !name of the folder to save the output files
-integer            :: fld_save_period=100!in time steps, saves Ex,Ey,Ez,Bx,By,Bz by default 
-integer            :: prtl_save_period=1000000!default: spatial cordinates: x,y,z;three-velocity: u,v,w; and q(charge),flv(species type)
-integer            :: spec_save_period=100!energy spectrum of the particles
-integer, parameter :: psave_ratio=128!(default ratio) 1 in every "psave_ratio" particles are tagged for tracking
+integer            :: fld_save_period=1!in time steps, saves Ex,Ey,Ez,Bx,By,Bz by default 
+integer            :: prtl_save_period=100000!default: spatial cordinates: x,y,z;three-velocity: u,v,w; and q(charge),flv(species type)
+integer            :: spec_save_period=1!energy spectrum of the particles
+integer, parameter :: psave_ratio=64!(default ratio) 1 in every "psave_ratio" particles are tagged for tracking
 integer            :: fsave_ratio=1!(default downsampling) field qunatities are saved in a reduced resolution by this factor
 logical            :: save_prtl_local_fld=.true.! to save the E abd B field at the particle's location 
 logical            :: save_prtl_local_curr=.false.! save the value of local current at the particle's location 
@@ -55,7 +55,7 @@ logical, parameter :: RZtwoD=.false. !no derivative along the theta-direction; R
 !------------------------------------------------------------------------------------------------
 
 !parameters for restarting the simulation from a saved state
-integer, parameter :: restart_save_period=10000000! period to save state of the simulation  
+integer, parameter :: restart_save_period=1000! period to save state of the simulation  
 
 ! parameters for performance optimisation
 integer, parameter :: prtl_reorder_period=20!sort particles in order to improve cache performance 
