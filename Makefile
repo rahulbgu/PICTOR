a13a09ebcf723f96753ce4de3ba21242d18d83a0
Rahul Kumar <rahuliitk@gmail.com>
2024-10-21 10:16:45 -0600

    Add files via upload

--- Makefile
+++ Makefile
@@ -3,33 +3,29 @@ SHELL=/bin/sh
 #---------------------------------------------------------------------------------------
 #  Configure the PIC simulation
 #---------------------------------------------------------------------------------------
-
-SETUP=setup_alfventurb
-SETUP=setup_shock
-#SETUP=setup_shear_flow
-SETUP=setup_Bell
-#SETUP=setup_cond_cyl_test
-SETUP=setup_bennett_pinch_cart
-#SETUP=setup_weibel_rel
-#SETUP=setup_harris_sheet
-#SETUP=setup_reconnection
-SETUP=setup_PUI_shock
-SETUP=setup_weibel
+SETUP=setup_ionize
+SETUP=setup_test_pml
 
 
 #THREE_DIMS=y# y = three spatial dimensions
 #CORD=cyl# cordinate system (default is Cartesian; 'cyl' is cylinderical)
 #OPEN_MP=y#to enable openmp support NOTE: include -qopenmp(intel) or -fopenmp (gcc) in "FLAGS"
-#GPU=y#Use GPUs  
+#GPU=y#Use GPUs
+
+# ----- Algorithm Selection  -----
+#FDTD=Lehe# options :  Lehe ; ( the standard Yee scheme is the default)
+
+
+
  
 #----------------------------------------------------------------------------------------  
 # settings for the compiler 
 #----------------------------------------------------------------------------------------
 cc= gcc#use "icc" for intel compilers 
-FC= h5pfc
+FC=h5pfc
 FLAGS= -O3 -cpp -fno-range-check -ffree-line-length-none -fallow-argument-mismatch#use to run the simulation using GCC compiler
 #FLAGS= -O3 -cpp -ipo -xHost#for intel compiler 
-#FLAGS= -O3 -g -fbacktrace -fcheck=all -cpp -fno-range-check -ffree-line-length-none -fbounds-check -fimplicit-none -fallow-argument-mismatch#-fimplicit-none # use for debugging 
+FLAGS= -O0 -g -finit-real=nan -Wmaybe-uninitialized -fbacktrace -fcheck=all -cpp -fno-range-check -ffree-line-length-none -fbounds-check -fimplicit-none -fallow-argument-mismatch#-fimplicit-none # use for debugging 
 #FLAGS= -O3 -ta=tesla:cc70 -Mcuda# for GPU runs (using PGI fortran) 
 
 
@@ -69,32 +65,44 @@ ifeq ($(GPU_USE_INTRINSICS),y)
 endif
 
 
+
+# -------- Algorithms -----------
+FDTD_SRC_EXT =# default
+ifeq ($(FDTD),Lehe)
+#	FDTD_SRC = em_update_lehe
+FDTD_SRC_EXT =_lehe# default
+endif
+
+
+
+
+
  
 FFLAGS= $(CUSTOM) $(FLAGS)
 EXECUTABLE= PICTOR
-VPATH=src:src/cyl
+VPATH=src:src/cyl:src/bc:src/io:src/maxwell:src/movdep:src/mem:src/init:src/comm:src/comm/subdomains:src/ionization
 OBJDIR=build
 
 ifeq ($(GPU),y)
-OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o var_gpu.o communication.o prtl_tag.o prob_dist.o memory.o interpolation.o prtl_stats.o reload.o initialise.o deposit.o deposit_gpu.o movdep.o comm_fldprtl.o usc_CFDTD_gpu.o usc_CFDTD.o bc_curved_surf.o loadprtlout_gpu.o particles.o em_update.o fields.o \
-	 HDF5write.o savedata_gpu.o savedata_routines.o comm_savedata.o comm_fld_gpu.o savedata.o help_setup.o DerivedQnty.o comm_prtl_gpu.o particles_gpu.o \
-	 interp_gpu.o em_update_gpu.o fields_gpu.o movdep_gpu.o initialise_gpu.o comm_loadbalance.o loadbalance_gpu.o loadbalance.o injector_gpu.o injector.o bc_gpu.o bc.o $(SETUP).o MAIN.o)
+OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o var_gpu.o communication.o comm_fld.o subdomains.o prtl_tag.o prob_dist.o mem_prtl.o mem_fld.o interpolation.o prtl_stats.o reload.o initialise.o deposit.o deposit_gpu.o loadprtlout.o movdep.o usc_CFDTD_gpu.o usc_CFDTD.o bc_curved_surf.o comm_prtl.o em_update.o fields.o \
+	 HDF5write.o particles_gpu.o savedata_gpu.o savedata_routines.o  comm_fld_gpu.o savedata.o help_setup.o DerivedQnty.o \
+	 interp_gpu.o em_update_gpu.o fields_gpu.o movdep_gpu.o initialise_gpu.o bc_exterior_field.o bc_open.o bc_attenuate.o bc_inflow.o bc_domain_size.o loadbalance_gpu.o loadbalance_indep.o bc_gpu.o bc.o $(SETUP).o MAIN.o)
 
 ifeq ($(CORD),cyl)
-OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o var_gpu.o cyl_vars.o communication.o prob_dist.o prtl_tag.o memory.o interpolation.o prtl_stats.o cyl_em_update.o deposit.o deposit_gpu.o movdep.o cyl_comm_fldprtl.o comm_fldprtl.o cyl_bc_gpu.o cyl_bc.o loadprtlout_gpu.o particles.o cyl_filter.o fields.o\
-	 HDF5write.o cyl_savedata_routines.o savedata_gpu.o savedata_routines.o comm_savedata.o cyl_comm_fld_gpu.o comm_fld_gpu.o savedata.o help_setup.o cyl_help_setup.o DerivedQnty.o reload.o initialise.o \
-	 comm_prtl_gpu.o particles_gpu.o interp_gpu.o cyl_em_update_gpu.o cyl_filter_gpu.o fields_gpu.o cyl_movdep_gpu.o initialise_gpu.o cyl_initialise.o comm_loadbalance.o loadbalance_gpu.o loadbalance.o cyl_loadbalance.o $(SETUP).o MAIN.o)
+OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o var_gpu.o cyl_vars.o communication.o subdomains.o prob_dist.o prtl_tag.o mem_prtl.o mem_fld.o interpolation.o prtl_stats.o cyl_em_update.o deposit.o deposit_gpu.o movdep.o cyl_comm_fld.o comm_fld.o cyl_bc_gpu.o cyl_bc.o loadprtlout_gpu.o comm_prtl.o cyl_filter.o fields.o\
+	 HDF5write.o cyl_savedata_routines.o savedata_gpu.o savedata_routines.o  cyl_comm_fld_gpu.o comm_fld_gpu.o savedata.o help_setup.o cyl_help_setup.o DerivedQnty.o reload.o initialise.o \
+	 particles_gpu.o interp_gpu.o cyl_em_update_gpu.o cyl_filter_gpu.o fields_gpu.o cyl_movdep_gpu.o initialise_gpu.o cyl_initialise.o bc_domain_size.o loadbalance_gpu.o loadbalance_indep.o cyl_loadbalance.o $(SETUP).o MAIN.o)
 endif
 
 
 else
 
 
-OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o communication.o prtl_tag.o prob_dist.o memory.o interpolation.o prtl_stats.o reload.o initialise.o deposit.o movdep.o comm_fldprtl.o injector.o usc_CFDTD.o bc_curved_surf.o bc.o loadprtlout.o particles.o em_update.o fields.o HDF5write.o\
-	 savedata_routines.o comm_savedata.o savedata.o help_setup.o DerivedQnty.o comm_loadbalance.o loadbalance.o $(SETUP).o MAIN.o)
+OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o mem_fld.o mem_prtl.o communication.o prtl_tag.o prob_dist.o comm_fld.o loadprtlout.o comm_prtl.o subdomains.o interpolation.o prtl_stats.o reload.o init_prtl.o initialise.o movdep_routines.o deposit.o movdep.o bc_exterior_field.o bc_reflect.o bc_open.o bc_adjust_speed.o bc_attenuate.o bc_pml.o bc_inflow.o usc_CFDTD.o bc_curved_surf.o bc.o em_update$(FDTD_SRC_EXT).o current.o HDF5write.o\
+	 read_data.o savedata_routines.o  savedata.o help_setup.o DerivedQnty.o bc_domain_size.o loadbalance_indep.o loadbalance_procgrid.o $(SETUP).o MAIN.o)
 ifeq ($(CORD),cyl)
-OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o cyl_vars.o communication.o prtl_tag.o prob_dist.o memory.o interpolation.o prtl_stats.o cyl_em_update.o deposit.o cyl_comm_fldprtl.o comm_fldprtl.o cyl_bc_cpu.o cyl_bc.o cyl_movdep.o loadprtlout.o particles.o \
-      cyl_filter.o fields.o HDF5write.o cyl_savedata_routines.o savedata_routines.o comm_savedata.o savedata.o help_setup.o cyl_help_setup.o DerivedQnty.o comm_loadbalance.o loadbalance.o cyl_loadbalance.o $(SETUP).o reload.o initialise.o cyl_initialise.o MAIN.o)
+OBJS= $(addprefix $(OBJDIR)/, parameters.o vars.o mem_fld.o mem_prtl.o cyl_common.o communication.o prtl_tag.o prob_dist.o comm_fld.o loadprtlout.o comm_prtl.o subdomains.o interpolation.o prtl_stats.o reload.o init_prtl.o initialise.o movdep_routines.o deposit.o cyl_movdep_strang.o bc_exterior_field.o bc_reflect.o bc_open.o bc_adjust_speed.o bc_attenuate.o bc_inflow.o usc_CFDTD.o bc_curved_surf.o bc.o cyl_em_update.o current.o HDF5write.o\
+	 read_data.o cyl_savedata_routines.o savedata_routines.o  savedata.o help_setup.o DerivedQnty.o bc_domain_size.o loadbalance_indep.o loadbalance_procgrid.o $(SETUP).o MAIN.o)
 endif
 
 endif 
@@ -107,7 +115,7 @@ $(OBJDIR)/%.o:%.cu
 	nvcc -c $^ -o $@
 
 $(EXECUTABLE): $(OBJS)
-	$(FC) $(FFLAGS) -o $@ $(OBJS)
+	$(FC) $(FFLAGS) $(LDFLAGS) -o $@ $(OBJS)
 	mv *.mod $(OBJDIR)/
 
 clean: 
--- parameters.F90
+++ parameters.F90
@@ -2,73 +2,88 @@ module parameters
 !----------------------------------
 !all user defined input parameters are listed here
 !---------------------------------- 
-integer, parameter :: psn=kind(1.0e0)!computation precision; 1.0e0: single precision, 1.0d0: double precision  
-integer, parameter :: nx=960!128!32*8!*16*8!no. of grid cells along x  
-integer, parameter :: ny=128!!32*8!no. of grid cells along y 
-integer, parameter :: nz=4!*16!no. of grid cells along z (used only if THREE_DIMS=y (3D simulation) in the Makefile)
-integer, parameter :: nSubDomainsX=2!*16!*2!32 ! no. of sub-domains along x
-integer, parameter :: nSubDomainsY=2!8!*2! no. of sub-domains along y
-integer, parameter :: nSubDomainsZ=1! no. of sub-domains along z (relevant only for 3D)
-integer, parameter :: tfinish=200000! maximum number of time steps
+integer, parameter :: psn=kind(1.0d0)	!computation precision; 1.0e0: single precision, 1.0d0: double precision  
+integer, parameter :: nx=2048		!no. of grid cells along x  
+integer, parameter :: ny=256	    !no. of grid cells along y 
+integer, parameter :: nz=4000		!no. of grid cells along z (used only if THREE_DIMS=y (3D simulation) in the Makefile)
+integer            :: nSubDomainsX=2	!*16!*2!32 ! no. of sub-domains along x
+integer            :: nSubDomainsY=4	!8!*2! no. of sub-domains along y
+integer            :: nSubDomainsZ=1	! no. of sub-domains along z (relevant only for 3D)
+integer, parameter :: tfinish=200000	! maximum number of time steps
+real(psn),parameter:: saveAndExitAfterHours = 2.0 !in hours (>0) :  stop execution, save restart data, and exit 
 
 ! physical parameters 
-real(psn), parameter :: g0=5.0_psn!if a relativistic plasma, g0 is the Lorentz factor of particles (determines the electron skin-depth) 
-integer, parameter   :: epc=32*2!number of electrons per cell 
-real(psn), parameter :: mi_me = 32.0_psn!mass ratio = ion mass/electron mass = mi/me 
-real(psn), parameter :: c_ompe=10.0_psn ! no. of cells per electron skin-depth 
-real(psn), parameter :: c=0.45_psn! *0.098 ! the speed of light (i.e., EM waves propagate about a distance c in one time step)
+real(psn), parameter :: g0=1.0_psn	!if a relativistic plasma, g0 is the Lorentz factor of particles (determines the electron skin-depth) 
+integer, parameter   :: epc=1		!number of electrons per unit cell of volume = 1 (dx=dy=dz=1), determines the electron charge
+real(psn), parameter :: mi_me=3671.0_psn!mass ratio = ion mass/electron mass = mi/me 
+real(psn), parameter :: c_ompe=30.0_psn! no. of cells per electron skin-depth 
+real(psn), parameter :: c=0.45_psn!0.98*0.707_psn! the speed of light (i.e., EM waves propagate about a distance c in one time step)
 real(psn), parameter :: EMspeedBoost=1.0_psn! EM waves are propagated faster than c by this factor 
-integer , parameter  :: curr_filter=0!3!no. of times the current filter (moving average) is applied  
+integer , parameter  :: curr_filter=0	!3!no. of times the current filter (moving average) is applied  
 integer , parameter  :: nMoverEMfilter=0!8!no. of times the EM fld averaging is performed, only relavant for the test particles 
+real(psn),parameter  :: ne0=1.20e19 ! (optional) electron density in cm^-3, sets physical units 
+
+!------------------------------------------------------------------------------------------------
+! Grid parameters
+!------------------------------------------------------------------------------------------------
+!grid spacing
+real(psn), parameter :: grid_dx = 1.0_psn
+real(psn), parameter :: grid_dy = 1.0_psn
+real(psn), parameter :: grid_dz = 1.0_psn
+
+!1D electrons per cell, (defines the 'even placement' of particles in a cell; epc = epc_x*epc_y*epc_z) 
+integer, parameter   :: epc_x = 1
+integer, parameter   :: epc_y = 1
+integer, parameter   :: epc_z = 2
 
 !------------------------------------------------------------------------------------------------
 ! Saving the output
 !------------------------------------------------------------------------------------------------
 ! Set the period to "-1" to disable saving a particular data, e.g. "fld_save_period=-1" will disble saving the field data   
-character(len=256) :: data_folder=trim("data") !name of the folder to save the output files
-integer            :: fld_save_period=1!in time steps, saves Ex,Ey,Ez,Bx,By,Bz by default 
-integer            :: prtl_save_period=1!00000!default: spatial cordinates: x,y,z;three-velocity: u,v,w; and q(charge),flv(species type)
-integer            :: spec_save_period=1!energy spectrum of the particles
-integer, parameter :: psave_ratio=64!(default ratio) 1 in every "psave_ratio" particles are tagged for tracking
-integer            :: fsave_ratio=1!(default downsampling) field qunatities are saved in a reduced resolution by this factor
+character(len=256) :: data_folder=trim("data") 	!name of the folder to save the output files
+integer            :: fld_save_period=1	!in time steps, saves Ex,Ey,Ez,Bx,By,Bz by default 
+integer            :: prtl_save_period=100	!00000!default: spatial cordinates: x,y,z;three-velocity: u,v,w; and q(charge),flv(species type)
+integer            :: spec_save_period=100	!energy spectrum of the particles
+integer, parameter :: psave_ratio=1	!(default ratio) 1 in every "psave_ratio" particles are tagged for tracking
+integer            :: fsave_ratio=1		!(default downsampling) field qunatities are saved in a reduced resolution by this factor
 logical            :: save_prtl_local_fld=.true.! to save the E abd B field at the particle's location 
 logical            :: save_prtl_local_curr=.false.! save the value of local current at the particle's location 
-logical            :: save_density=.true. ! density of particles
-logical            :: save_ch_flux_fld=.true. ! flux of the charge(q), i.e., q<v_i>, i=x,y,z
-logical            :: save_velsq_fld=.false. ! q<v^2_i>, i=x,y,z
-logical            :: save_tot_curr=.true. ! the net current, the exact quantiites used in the Maxwell's equations
-logical            :: save_divE=.false. ! divergence of electric field
-logical            :: save_EdotV_fld=.false. ! <E.V>, averaging is done over cells of size fsave_ratio
-logical            :: save_eng_spat=.false. ! total energy of particles in cells of size fsave_ratio
+logical            :: save_density=.true. 	! density of particles
+logical            :: save_ch_flux_fld=.true. 	! flux of the charge(q), i.e., q<v_i>, i=x,y,z
+logical            :: save_velsq_fld=.false. 	! q<v^2_i>, i=x,y,z
+logical            :: save_tot_curr=.true. 	! the net current, the exact quantiites used in the Maxwell's equations
+logical            :: save_divE=.false. 	! divergence of electric field
+logical            :: save_EdotV_fld=.false. 	! <E.V>, averaging is done over cells of size fsave_ratio
+logical            :: save_eng_spat=.false. 	! total energy of particles in cells of size fsave_ratio
 !The following parameters are useful to save various useful qunatities 
-integer            :: prtl_mean_save_period=-1 !serval mean quantitities(e.q., E.V), but now averaged over all the particles
-integer, parameter :: restart_save_period=1000! period to save state of simulation (checkpoint) to restart  
+integer            :: prtl_mean_save_period=-1 	!several mean quantitities(e.q., E.V), but now averaged over all the particles
+integer, parameter :: restart_save_period=1000	! period to save state of simulation (checkpoint) to restart  
 
 !------------------------------------------------------------------------------------------------
-!Cylinderical grid (coordiante system must be changed in Makefile as well, Default is Cartesian)  
+!Cylinderical grid (coordiante system must be changed in "Makefile" as well, default is Cartesian)  
 !------------------------------------------------------------------------------------------------
-logical, parameter :: inc_axis=.true. ! Incude the axis in the domain, no inner BC 
-logical, parameter :: RZtwoD=.false. !no derivative along the theta-direction; RZ -plane only; see cyl_em_update* (GPU version only)  
+logical, parameter :: inc_axis=.true. 	! Incude the axis in the domain, no inner BC 
+logical, parameter :: RZtwoD=.false. 	!no derivative along the theta-direction; RZ -plane only; see cyl_em_update* (GPU version only)  
 
 !------------------------------------------------------------------------------------------------
 !Advanced settings  :: Performance
 !------------------------------------------------------------------------------------------------
-integer, parameter :: prtl_reorder_period=20!sort particles in order to improve cache performance 
-integer, parameter :: performance_save_period=10 !period to save performance of each core
-integer, parameter :: load_balance_period=120!(optional) time period to change the size of sub-domains to balance the load  
+integer, parameter :: prtl_reorder_period=20		!sort particles in order to improve cache performance 
+integer, parameter :: performance_save_period=100000 	!period to save performance of each core
+integer, parameter :: load_balance_period=120	!(optional) time period to change the size of sub-domains to balance the load 
+integer            :: load_balance_yz_period = 120 !(optional) change the number of proc. along x-axis to balance the load across yz axes
 
 !------------------------------------------------------------------------------------------------
 !Advanced settings  :: Particle spectrum
 !------------------------------------------------------------------------------------------------
-integer,parameter  :: prtl_spec_type=3 !1: only gamma spectrum, 2: only speed spectrum, 3 :both, gamma and speed   
-real   ,parameter  :: Gamma_spec_binwidth=0.1/10!dlog(Gamma)
-real   ,parameter  :: Speed_spec_binwidth=0.001 !d speed 
+real   ,parameter  :: Gamma_spec_binwidth=0.1/10	!dlog(Gamma)
+real   ,parameter  :: Speed_spec_binwidth=0.001 	!d speed/c;  
 
 !------------------------------------------------------------------------------------------------
 !Advanced settings  :: GPU
 !------------------------------------------------------------------------------------------------
-integer, parameter :: gpu_prtl_arr_size=512000000 ! max. no. of particles on GPU
-logical            :: curr_filter_gpu=.false.!.ture.= filtering on GPU;.false. use the host filtering routines
+integer, parameter :: gpu_prtl_arr_size=512000000 	! max. no. of particles on GPU
+logical            :: curr_filter_gpu=.false.		!.true.= filtering on GPU;.false. use the host filtering routines
 
 
 
